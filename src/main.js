94356f5d1e331743c660d16cfe12094d8635e662
HcHh2019 <2533313507@qq.com>
2021-05-18 11:37:41 +0800

    环境变量配置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 
 import Vue from 'vue'
 import router from './router'
-
 import axios from 'axios'
+import env from '../env'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
 Vue.prototype.axios = axios
 // 根据前端方式调整，这里用的是接口代理,使用的是‘/api’
 axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000;
+// 根据环境变量获取不同的请求地址
+axios.defaults.baseURL = env.baseURL;
 // 接口错误拦截
 axios.interceptors.response.use(function(response){
   let res = response.data;
@@ -19,16 +25,7 @@ axios.interceptors.response.use(function(response){
   }
 })
 
-
-
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-
-
 new Vue({
   router,
-  axios,
   render: h => h(App),
 }).$mount('#app')

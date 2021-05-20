8f842b00881348bff2918bed8d0b26f23413acba
HcHh2019 <2533313507@qq.com>
2021-05-20 11:39:35 +0800

    添加SwiperSlidec组件

---
+++ src/components/SwiperSlidec.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <swiper :options="swiperOption">
+          <swiper-slide v-for="(item,index) in slideList" :key="index">
+            <a :href="'/#/product/'+item.id"><img :src="item.img"></a>
+          </swiper-slide>
+           <!-- Optional controls -->
+          <div class="swiper-pagination"  slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import {Swiper,SwiperSlide} from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+export default {
+ name: 'swiper-slidec',
+ components: {
+   Swiper,
+   SwiperSlide
+ },
+ data() {
+   return {
+     swiperOption: {
+       autoplay: true,
+       loop: true,
+       effect : 'cube',
+       cubeEffect: {
+         slideShadows: false,
+         shadow: false,
+         shadowOffset: 0,
+         shadowScale: 0.6
+       },
+       pagination: {
+        el: '.swiper-pagination',
+        clickable:true
+       },
+       navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+       }
+     },
+     slideList: [
+       {
+         id:'42',
+         img: '/imgs/slider/slide-1.jpg'
+       },
+       {
+         id:'45',
+         img: '/imgs/slider/slide-2.jpg'
+       },
+       {
+         id:'46',
+         img: '/imgs/slider/slide-3.jpg'
+       },
+       {
+         id:'',
+         img: '/imgs/slider/slide-4.jpg'
+       },
+       {
+         id:'',
+         img: '/imgs/slider/slide-5.jpg'
+       }
+     ]
+   }
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+  .swiper-box {
+    .swiper-container {
+      height: 451px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,19 +1,41 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper-slidec></swiper-slidec>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
     <service-bar></service-bar>
   </div>
 </template>
 
 <script>
 import ServiceBar from '../../components/ServiceBar'
+import SwiperSlidec from '../../components/SwiperSlidec'
+
 export default {
  name: 'index',
  components: {
    ServiceBar,
+   SwiperSlidec,
+ },
+ data() {
+   return {}
  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .index {
+    .container {
+      position: relative;
+      width: 1226px;
+      margin-left: auto;
+      margin-right: auto;
 
+    }
+  }
 </style>
